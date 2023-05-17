315905f9801bcb45ca079c023b7e4cf91a042a34
Fermata-YOON <98hsyoon@naver.com>
2023-05-17 17:10:51 +0900

    post complete

--- server_test.js
+++ server_test.js
@@ -1,45 +1,90 @@
 const mongoose = require('mongoose');
+const http = require('http');
+const express = require('express');
+const app = express();
+const path = require('path');
+const db = main()
 
-main()
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+const mySchema = new mongoose.Schema({
+    time: String,
+    count: Number, //현재인원
+    maxCount: Number, //최대탑승인원
+    link: String, //채팅링크
+    create: String, //게시글 생성일
+    arrival: String, //출발지
+    departure: String, //목적지
+    password: String, //글 비밀번호
+})
+
+const Article = mongoose.model('Article', mySchema)
+
+
+function postArticle(data) {
+    const today = new Date()
+    const year = today.getFullYear()
+    const month = today.getMonth()
+    const date = today.getDate()
+
+    const article = new Article
+
+    article.time = data.hour + '/' + data.minute
+    article.count = 1
+    article.maxcount = data.people
+    article.link = data.url
+    article.create = year + "-" + month + "-" + date
+    article.arrival = data.from
+    article.departure = data.to
+    article.password = data.password
+
+    return article
+}
+
+function getList() {
+    const articles = Article.find({});
+
+    return articles
+}
 
 async function main() {
-    await mongoose.connect("mongodb+srv://hyunsub:60171914@cluster0.8a0ppgx.mongodb.net/", {
+    const db = await mongoose.connect("mongodb+srv://hyunsub:60171914@cluster0.8a0ppgx.mongodb.net/", {
         dbName: 'test'
     })
         .then(() => console.log('connected'))
         .catch((err) => console.log('connect fail'))
 
-    mySchema = new mongoose.Schema({
-        title: String, //글 제목
-        time: String, //탑승시간
-        count: Number, //탑승인원
-        link: String, //채팅링크
-        create: String, //게시글 생성시간
-        arrival: String, //출발지
-        departure: String, //목적지
-        password: String //글 비밀번호
-    })
-
-    const Article = mongoose.model('Article', mySchema)
-
-    var test_article = new Article({
-        title: '명지택시', //글 제목
-        time: '1000', //탑승시간
-        count: 1, //탑승인원
-        link: 'open.link.test', //채팅링크
-        create: '0900', //게시글 생성시간
-        arrival: '명지대', //출발지
-        departure: '기흥역', //목적지
-        password: '0000' //글 비밀번호
-    })
-
-    console.log(test_article.title)
-
-    await test_article.save()
+    /*await test_article.save()
         .then(() => console.log("성공"))
         .catch((err) => console.log("실패"))
 
     const articles = await Article.find()
-    console.log(articles)
+    console.log(articles)*/
+
+// 서버 시작
+    app.listen(3000, () => {
+        console.log('서버가 3000 포트에서 실행 중입니다.');
+    });
+
+    return db
 }
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'prototype.html'));
+});
+
+app.post('/article', (req, res) => {
+    const newArticle = postArticle(req.body)
+    newArticle.save()
+        .then(res.json({"msg": "success"}))
+        .catch((err)=>{res.json({"msg": "error"})})
+});
+
+app.get('/list', (req, res) => {
+    const articles = getList()
+    console.log(articles)
+    res.json(articles)
+})
 

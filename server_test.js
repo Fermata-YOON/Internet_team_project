663ba9b9ae572bd7abf6fc68d3773d93596c53e8
Fermata-YOON <98hsyoon@naver.com>
2023-05-23 11:43:06 +0900

    version 1.0.0

--- server_test.js
+++ server_test.js
@@ -3,14 +3,15 @@ const http = require('http');
 const express = require('express');
 const app = express();
 const path = require('path');
-const db = main()
+
+main()
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 const mySchema = new mongoose.Schema({
-    time: String,
+    time: String, //글 작성 시간
     count: Number, //현재인원
     maxCount: Number, //최대탑승인원
     link: String, //채팅링크
@@ -18,9 +19,10 @@ const mySchema = new mongoose.Schema({
     arrival: String, //출발지
     departure: String, //목적지
     password: String, //글 비밀번호
+    studentNum: [] //학번
 })
 
-const Article = mongoose.model('Article', mySchema)
+let Article = mongoose.model('Article', mySchema)
 
 
 function postArticle(data) {
@@ -31,22 +33,30 @@ function postArticle(data) {
 
     const article = new Article
 
-    article.time = data.hour + '/' + data.minute
+    article.time = data.hour + '시' + data.minute + '분'
     article.count = 1
-    article.maxcount = data.people
-    article.link = data.url
+    article.maxCount = data.people
+    article.link = data.talk
     article.create = year + "-" + month + "-" + date
     article.arrival = data.from
     article.departure = data.to
     article.password = data.password
+    article.studentNum = data.studentNum
 
     return article
 }
 
-function getList() {
-    const articles = Article.find({});
-
-    return articles
+async function getList() {
+    await Article.find({}, (err, articles) => {
+        if (err) {
+            console.error('게시물 중 오류가 발생했습니다:', err);
+        } else {
+            console.log('게시물 목록:');
+            articles.forEach((article) => {
+                console.log(article)
+            });
+        }
+    })
 }
 
 async function main() {
@@ -82,9 +92,42 @@ app.post('/article', (req, res) => {
         .catch((err)=>{res.json({"msg": "error"})})
 });
 
-app.get('/list', (req, res) => {
-    const articles = getList()
-    console.log(articles)
+app.delete('/delete_article', async (req, res) => {
+    const id = req.body.id
+    const password = req.body.password
+    const myquery = {'_id': id, 'password': password}
+
+    const article = await Article.find({'_id': id, 'password': password})
+
+    if(article.length != 0) {
+        Article.deleteOne({'_id': id})
+            .then(res.json({'msg':'삭제완료'}))
+            .catch((err)=>{res.json({'msg': '에러가 발생했습니다.'})})
+    } else {
+        res.json({'msg': '비밀번호가 일치하지 않습니다!'})
+    }
+});
+
+app.get('/list', async (req, res) => {
+    let articles = await Article.find({})
+    //console.log(articles)
     res.json(articles)
 })
 
+app.post('/link', async (req, res) => {
+    const id = req.body.id
+    const studentNum = req.body.studentNum
+    const article = await Article.find({'_id': id})
+    console.log(article[0]['studentNum'])
+
+    if(article[0]['studentNum'].includes(studentNum)) {
+        console.log('student in article')
+        res.json(article)
+    } else {
+        console.log('push student')
+        await Article.updateOne({'_id': id},{$push: {'studentNum': studentNum}, $inc:{'count': 1}})
+            //.then(console.log('update success'))
+            //.catch((err)=>{console.log('update fail')})
+        res.json(article)
+    }
+})
